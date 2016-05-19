d96ebd3fda484b03c52f53144f394ac946ba3ce2
kalaiprakash93 <kalaiprakash93@gmail.com>
2016-05-19 10:20:48 +0530

    Delete LocationPlugin.js

--- www/LocationPlugin.js
+++
@@ -1,8 +0,0 @@
-var exec = require('cordova/exec');
-
-function LocationPlugin() { 
- Toast.makeText(getApplicationContext(), connection", Toast.LENGTH_SHORT).show();
-}
-	exec(isServiceRunning, errorLogger, 'LocationPlugin', 'isServiceRunning', []);
- var locationPlugin = new LocationPlugin();
- module.exports = locationPlugin;
